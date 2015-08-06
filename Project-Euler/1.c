a8919d2ebcfacd2c214f5844922b085d9e9a5843
Majon-Thanaton <randomnamehere@live.com.au>
2015-08-07 01:19:01 +1000

    Need some practice with C...

--- Project-Euler/1.c
+++ Project-Euler/1.c
@@ -1,2 +1,10 @@
-This is a test.
+/*[This is a WIP. First time Git user, and sucky mathematician. But hey, doing better than not trying.]*/
+
+/*Problem 1; Multiples of 3 & 5.
+Find the sum of all multiples of 3 or 5 below 100.
+
+Recursive function to check if number is factor of 3 or 5.
+If it is, add to sum, if not, ignore.
+*/
+
 
